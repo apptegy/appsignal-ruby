745089d645c1217bd299e7db6b8eb44c0aae27c6
Thijs Cadier <thijs@appsignal.com>
2020-03-31 12:24:27 +0200

    Support filtered data in extension

--- lib/appsignal/config.rb
+++ lib/appsignal/config.rb
@@ -79,6 +79,7 @@ module Appsignal
       "APPSIGNAL_FILES_WORLD_ACCESSIBLE"         => :files_world_accessible,
       "APPSIGNAL_REQUEST_HEADERS"                => :request_headers,
       "APPSIGNAL_TRANSACTION_DEBUG_MODE"         => :transaction_debug_mode,
+      "APPSIGNAL_FILTER_DATA_KEYS"               => :filter_data_keys,
       "APP_REVISION"                             => :revision
     }.freeze
     # @api private
@@ -124,6 +125,7 @@ module Appsignal
       APPSIGNAL_IGNORE_ERRORS
       APPSIGNAL_IGNORE_NAMESPACES
       APPSIGNAL_REQUEST_HEADERS
+      APPSIGNAL_FILTER_DATA_KEYS
     ].freeze
 
     # Mapping of old and deprecated AppSignal configuration keys
@@ -302,6 +304,7 @@ module Appsignal
       ENV["_APPSIGNAL_FILES_WORLD_ACCESSIBLE"]       = config_hash[:files_world_accessible].to_s
       ENV["_APPSIGNAL_TRANSACTION_DEBUG_MODE"]       = config_hash[:transaction_debug_mode].to_s
       ENV["_APPSIGNAL_SEND_ENVIRONMENT_METADATA"]    = config_hash[:send_environment_metadata].to_s
+      ENV["_APPSIGNAL_FILTER_DATA_KEYS"]             = config_hash[:filter_data_keys].join(',')
       ENV["_APP_REVISION"]                           = config_hash[:revision].to_s
     end
 
